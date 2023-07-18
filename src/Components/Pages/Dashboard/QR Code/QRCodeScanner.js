d546e0e38059279451e8c200ca561ad6d1e9ce11
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-18 15:32:57 +0600

    edit

--- src/Components/Pages/Dashboard/QR Code/QRCodeScanner.js
+++ src/Components/Pages/Dashboard/QR Code/QRCodeScanner.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import { AiFillCamera, AiOutlineAppstoreAdd } from 'react-icons/ai';
+import { AiFillCamera } from 'react-icons/ai';
+import { CgPushChevronUpR } from 'react-icons/cg';
 import { QrReader } from 'react-qr-reader';
 
 const QRCodeScanner = () => {
   const [data, setData] = useState(false);
   const [open, setOpen] = useState(false);
+  const handleSubmit = () => {
+    setData(false);
+    setOpen(false);
+  };
   return (
     <div className="mt-14">
       <div className="flex justify-center gap-10">
@@ -12,21 +17,27 @@ const QRCodeScanner = () => {
           {' '}
           <h1 className="text-2xl text-center font-bold py-1">Scan Here</h1>
           {open ? (
-            <div className="">
-              <QrReader
-                className="-mt-10 rounded-xl"
-                onResult={(result, error) => {
-                  if (!!result) {
-                    setData(result?.text);
-                  }
+            <>
+              {data ? (
+                <h1>{data}</h1>
+              ) : (
+                <div>
+                  <QrReader
+                    className="-mt-10 rounded-xl"
+                    onResult={(result, error) => {
+                      if (!!result) {
+                        setData(result?.text);
+                      }
 
-                  if (!!error) {
-                    console.info(error);
-                  }
-                }}
-                style={{ width: '100%' }}
-              />
-            </div>
+                      if (!!error) {
+                        console.info(error);
+                      }
+                    }}
+                    style={{ width: '100%' }}
+                  />
+                </div>
+              )}
+            </>
           ) : (
             <div>
               <img
@@ -39,19 +50,30 @@ const QRCodeScanner = () => {
         </div>
         <div className="flex justify-center items-center w-[350px] h-[300px]  rounded-xl shadow-2xl shadow-blue-800 mt-10">
           <div>
-            <button onClick={() => setOpen(true)} className="btn btn-primary">
-              {' '}
-              <AiFillCamera className="mr-1 text-xl" />
-              Scan For Click
-            </button>
-            <button onClick={() => setOpen(true)} className="btn btn-primary">
-              {' '}
-              <AiOutlineAppstoreAdd className="mr-1 text-xl" />
-              Add Product
-            </button>
+            {data ? (
+              <button onClick={handleSubmit} className="btn btn-primary">
+                {' '}
+                <CgPushChevronUpR className="mr-1 text-xl" />
+                submit
+              </button>
+            ) : (
+              <button onClick={() => setOpen(true)} className="btn btn-primary">
+                {' '}
+                <AiFillCamera className="mr-1 text-xl" />
+                Scan For Click
+              </button>
+            )}
           </div>
         </div>
       </div>
+      {/* <div className="flex justify-center mt-14">
+        {data && (
+          <div className="w-[300px] h-[180px] shadow-2xl shadow-blue-800 bg-red-500 rounded-xl">
+            <h1>{data}</h1>
+          </div>
+        )}
+      </div> */}
+      <h1>{data}</h1>
     </div>
   );
 };
